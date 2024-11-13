dbaef23f4d76697bd949129a24098e40f9e45709
Praew2003 <stu6512732115@sskru.ac.th>
2024-11-13 11:12:53 +0700

    up

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,21 @@
-import React from "react";
-import UserInfo from "./userInfo";
-import { Fatname,Motname,Age,Testcount } from "./sumfunc";
+// import React from "react";
+// import UserInfo from "./userInfo";
+// import { Fatname,Motname,Age,Testcount } from "./sumfunc";
 
-const App=()=> {
-    return (
-        <div>
-            <h1 style={{color:'blue'}}>สวัสดี</h1>
-            <font size='100' color='#ff0099'>มะม่วงสุกดี</font>
-            <UserInfo />
-            <h1><u><Fatname /></u></h1>
-            <h1><u><Motname /></u></h1>
-            <Age age='30' year='1997'/>
-            <Testcount />
-        </div>
-    );
-}
+// const App=()=> {
+//     return (
+//         <div>
+//             <h1 style={{color:'blue'}}>สวัสดี</h1>
+//             <font size='100' color='#ff0099'>มะม่วงสุกดี</font>
+//             <UserInfo />
+//             <h1><u><Fatname /></u></h1>
+//             <h1><u><Motname /></u></h1>
+//             <Age age='30' year='1997'/>
+//             <Testcount />
+//         </div>
+//     );
+// }
 
-export default App;
+// export default App;
 
 
